4c8211f0d273709b23eb5cc1b48a00b1639eae57
webmaster <servernote.net@gmail.com>
2023-02-22 21:51:42 +0900

    update models

--- openai_api_list_models.js
+++ openai_api_list_models.js
@@ -6,7 +6,10 @@ const openai = new OpenAIApi(configuration);
 
 (async () => {
 	var response = await openai.listModels();
-	console.dir(response, { depth: null });
+	//console.dir(response, { depth: null });
+	response.data.data.forEach(function (d) {
+		console.log(d.id);
+	});
 	console.log('Total Models: ' + response.data.data.length);
 })();
 
